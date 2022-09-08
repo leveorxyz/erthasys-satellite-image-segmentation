9fd873fb86d1eb4c2eae8661dd90527e1412e581
Atick Faisal <atickfaisal@gmail.com>
2022-09-08 18:07:02 +0300

    Fix base64 conversions

--- utils/base64utils.py
+++ utils/base64utils.py
@@ -1,13 +1,16 @@
+import io
 import base64
 import numpy as np
 
+from PIL import Image
 from numpy.typing import NDArray
 
 
-def numpy_to_base64(image: NDArray[np.uint8]) -> bytes:
-    return base64.b64encode(image)
+def numpy_to_base64(image: NDArray[np.uint8]) -> str:
+    return base64.b64encode(image).decode("utf-8")
 
 
-def base64_to_numpy(image: bytes, shape: tuple[int, int, int]) -> NDArray[np.uint8]:
-    decoded_image = np.frombuffer(base64.b64decode(image), np.uint8)
-    return decoded_image.reshape(shape)
+def base64_to_numpy(image: str) -> NDArray[np.uint8]:
+    base64_decoded = base64.b64decode(image)
+    image = Image.open(io.BytesIO(base64_decoded))
+    return np.array(image, dtype=np.uint8)
